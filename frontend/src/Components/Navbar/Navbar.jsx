07ffc3430db100e1270964b24123d9bcb46947c8
KavinduGD <it21038914@my.sliit.lk>
2024-02-04 16:40:58 +0530

    40

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [menu, setmenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -11,17 +14,51 @@ function Navbar() {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>
-          Shop
-          <hr />
-        </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setmenu("shop");
+            }}
+          >
+            Shop
+            {menu === "shop" && <hr />}
+          </li>
+        </Link>
+        <Link to="men" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setmenu("men");
+            }}
+          >
+            Men {menu === "men" && <hr />}
+          </li>
+        </Link>
+        <Link to="women" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setmenu("women");
+            }}
+          >
+            Women {menu === "women" && <hr />}
+          </li>
+        </Link>
+        <Link to="kids" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setmenu("kids");
+            }}
+          >
+            Kids {menu === "kids" && <hr />}
+          </li>
+        </Link>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link to="login">
+          <button>Login</button>
+        </Link>
+        <Link to="cart">
+          <img src={cart_icon} alt="" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
